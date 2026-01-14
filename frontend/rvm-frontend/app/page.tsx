e0e02a095beee6ae128d0b9fe2c9f0ef3eb8eca5
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2026-01-14 18:15:13 +0800

    Created idle page

--- frontend/rvm-frontend/app/page.tsx
+++ frontend/rvm-frontend/app/page.tsx
@@ -2,63 +2,55 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="flex min-h-screen items-center justify-center font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col p-8 bg-gray-200 gap-3">
+        <div className="flex flex-row justify-center gap-3">
+          <div className="flex rounded-4xl bg-[#3B9549] px-6 pt-10 overflow-hidden w-[25vh] h-[35vh]">
+            <div className="flex rounded-t-3xl justify-center pt-6 pb-4 bg-[#114e1a] w-full h-full">
+              <Image
+                src="/images/Bottle.png"
+                alt="Bottle"
+                width={150}
+                height={200}
+                className="object-contain"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col text-[#114E1A] text-center border-2 p-3 rounded-3xl w-[25vh] h-[35vh] justify-center gap-4">
+            <h1 className="font-bold">PUT BOTTLE INSIDE COMPARTMENT</h1>
+            <hr />
+            <p className="text-xs">Ibutang ang botilya sa suludlan</p>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center w-full p-4 bg-linear-to-r from-[#175520] to-[#91C14D] rounded-4xl">
+          <h1 className="font-extrabold">ONE BOTTLE AT A TIME</h1>
+        </div>
+
+        <div className="flex items-center justify-center w-full">
+          <Image
+            src="/images/arrows.png"
+            alt="Stack of bottles"
+            width={100}
+            height={190}
+            className="object-contain"
+          />
+        </div>
+
+        <div className="flex flex-col text-[#114E1A] text-center border-2 p-3 rounded-3xl w-full justify-center gap-4ss">
+          <p className="text-[3vh] font-medium">
+            Note: Only 500ml plastic bottles are accepted
           </p>
+          <p className="text-[2vh]">Property of Green Module Systems</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+
+        <a
+          href=""
+          className="flex items-center justify-center w-full p-4 bg-[#078C10] rounded-4xl"
+        >
+          <h1 className="font-extrabold text-xl">SCAN BOTTLE</h1>
+        </a>
       </main>
     </div>
   );
